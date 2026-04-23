b340f861ba7c9c3ea7282d63e8701aeed84a385f
IR-Abenido <ianrafaelabenido@gmail.com>
2026-04-23 18:47:16 +0800

    Added patient relationship

--- app/Models/Facility.php
+++ app/Models/Facility.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 #[Fillable(['name', 'address', 'user_id'])]
 class Facility extends Model
@@ -12,5 +13,10 @@ class Facility extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function patients(): HasMany
+    {
+        return $this->hasMany(Patient::class);
     }
 }
