6471f225023e2c5ca30a0fcd392b069eed81874b
IR-Abenido <ianrafaelabenido@gmail.com>
2026-04-27 17:45:26 +0800

    Updated api and web routes

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Http\Controllers\API\AppointmentController;
+use App\Http\Controllers\API\ConsultationRecordController;
 use App\Http\Controllers\API\FacilityController;
 use App\Http\Controllers\API\PatientAllergyController;
 use App\Http\Controllers\API\PatientController;
+use App\Http\Controllers\API\PrescriptionController;
 use App\Http\Controllers\API\ScheduleController;
-
+use App\Http\Controllers\API\VitalSignController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
@@ -13,5 +15,8 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('facilities.patients', PatientController::class);
     Route::apiResource('patients.allergies', PatientAllergyController::class);
     Route::apiResource('patients.appointments', AppointmentController::class);
-    Route::apiResource('schedules', ScheduleController::class);
+    Route::apiResource('users.schedules', ScheduleController::class);
+    Route::apiResource('appointments.consultations', ConsultationRecordController::class);
+    Route::apiResource('consultations.prescriptions', PrescriptionController::class);
+    Route::apiResource('consultations.vitals', VitalSignController::class);
 });
